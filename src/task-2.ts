675c18c01f22af8f63bb9b2c0908f26686466211
YanaDem1601 <yanadem160180@gmail.com>
2026-06-04 02:29:40 +0300

    t2-1

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,4 +1,10 @@
-const product: { id: number; title: string; description?: string } = {
+interface Product {
+  readonly id: number;
+  title: string;
+  description?: string;
+}
+
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
